84aa3ae72e1006c2981e07d9d7907b0ff4ab56c8
yanickrochon <yanick.rochon@gmail.com>
2014-04-24 15:05:52 -0400

    All tests pass. Version bumped

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 Role-Based Access Control for [Koa](https://github.com/koajs/koa)
 
+This module follows the [NIST RBAC model](http://en.wikipedia.org/wiki/NIST_RBAC_model)
+and offer a flexible solution to allow or restrict user operations.
+
 
 ## Install
 
@@ -10,40 +13,126 @@ npm install koa-rbac --save
 ```
 
 
+## Introduction
+
+In an RBAC system, permissions are assigned to roles, not users. Therefore, roles
+act as a ternary relation between permissions and users. Permissions are static,
+defined in the applications. Roles, on the other hand, are dynamic and can be
+defined from an application interface and saved in a datastore.
+
+This module is not dependent on a authentication, a user session, or a datastore
+system. The relation between the user and it's roles are specified by an
+`AccessProvider`. It is the application's responsibility to implement such provider,
+which requires at least only two methods; `getRolePermissions` and `getUserAccess`,
+two methods returning the application's roles configuration and current user's roles
+assignments respectively.
+
+Rules are applied in consideration with the roles hierarchy. Top level roles always
+have priority over inherited roles. This measn that, for example, given two roles :
+`reader` and `editor`, respectively assigned the permissions `read` and `update`, and
+where `editor` inherits from `reader`, a rule allowing `read`, but denying `update`
+will validate if a user is an `editor`, but not a `reader`. If, `read` is instead
+directly assigned to the `editor` role (inheriting `reader` or not), then the previous
+rule would not validate for either an `editor` and a `reader` because `read` would
+be a top level permission and has equal weight with the allowed permission.
+
+When declaring rules on a resource, **allow** rules must be applied *before* the **deny**
+ones; as any non-validating rule which do not have a greater weight than a valid
+one will cause a `403 - Forbidden` error to be thrown.
+
+
 ## Usage
 
 ```javascript
+// index.js
+
 var rbac = require('koa-rbac');
 var koa = require('koa');
 var app = koa();
+var AccessProvider = require('./access-provider');
 
-
-rbac.addRoles({
-  'guest': [],
-  'reader': ['read'],
-  'writer': ['write'],
-});
-
-// parent -> child
-rbac.setRoleRelationship('reader', 'guest');
-rbac.setRoleRelationship('writer', 'reader');
-
-
-app.use(rbac.middleware({
-  loadUserAccess: loadUserAccess
-}));
-
-app.use(rbac.allow('read'));
-
+app.use(rbac.middleware(new AccessProvider()));
+app.use(rbac.allow('update'));
+app.use(rbac.deny('read'));
 app.use(function * (next) {
-  this.body = "Allowed reading!";
+  this.body = "Allowed updating but not reading!";
 });
+```
 
+```javascript
+// access-provider.js
 
-function * loadUserAccess() {
+/**
+Role cache. Normally, these would be stored in a database, or something
+*/
+var roles = {
+  'guest': {
+    name: 'Guest'
+  },
+  'reader': {
+    name: 'Reader',
+    permissions: ['read'],
+    inherits: ['guest']
+  },
+  'writer': {
+    name: 'Writer',
+    permissions: ['create'],
+    inherits: ['reader']
+  },
+  'editor': {
+    name: 'Editor',
+    permissions: ['update'],
+    inhertis: ['reader']
+  },
+  'director': {
+    name: 'Director',
+    permissions: ['delete'],
+    inherits: ['reader', 'editor']
+  },
+  'admin': {
+    name: 'Administrator',
+    permissions: ['manage']
+  }
+};
+
+module.exports = AccessProvider;
+
+/**
+AccessProvider constructor
+*/
+function AccessProvider() {
+  this.getRolePermissions = getRolePermissions;
+  this.getUserRoles = getUserRoles;
+}
+
+/**
+Return the role defined by `roleName`
+*/
+function * getRolePermissions(roleName) {
+  // NOTE : the method should return an object similar to the one described here
+  return roles[roleName];
+}
+
+/**
+Return the roles assigned to the user. Each item of the returned array will
+invoke `getRole` with it's item value
+*/
+function * getUserRoles(ctx) {
   return ['guest', 'reader'];
 }
 ```
+
+
+## Super User Role (Administrator)
+
+This RBAC module does not have a built-in "administrator" role or permission. Such
+privileged role must be implemented by the application. This can be achieved with
+a role (ex: `admin`), with no inheritance, with a special permission (ex: `manage`),
+and allow this special permission only and on every resource that has an *allow*`
+or *deny* rule set.
+
+This way, the special permission (ex: `manage`) can be assigned on other roles as
+well, but may be denied, too, if necessary.
 
 
 ## Contribution
--- test/lib/rbac.test.js
+++ test/lib/rbac.test.js
@@ -2,6 +2,210 @@
 
 describe('Test RBAC', function () {
 
-  it('should register roles');
+  var request = require('supertest');
+  var koa = require('koa');
+  var rbac = require('../../lib/rbac');
+
+  var roles = {
+    'guest': {
+      name: 'Guest',
+      permissions: ['foo']
+    },
+    'reader': {
+      name: 'Reader',
+      permissions: ['read'],
+      inherits: ['guest']
+    },
+    'writer': {
+      name: 'Writer',
+      permissions: ['create'],
+      inherits: ['reader']
+    },
+    'editor': {
+      name: 'Editor',
+      permissions: ['update'],
+      inherits: ['reader']
+    },
+    'director': {
+      name: 'Director',
+      permissions: ['delete'],
+      inherits: ['editor']
+    },
+    'admin': {
+      name: 'Administrator',
+      permissions: ['manage']
+    }
+  };
+
+  var userRoles = {
+    'bart': ['guest', 'reader'],
+    'marge': ['editor'],
+    'homer': ['admin', 'director'],
+    'burns': ['admin']
+  };
+
+  var accessProvider = new function AccessProvider() {
+    this.getRolePermissions = getRolePermissions;
+    this.getUserRoles = getUserRoles;
+  }
+
+  /**
+  Return the role defined by `roleName`
+  */
+  function * getRolePermissions(roleName) {
+    // NOTE : the method should return an object similar to the one described here
+    return roles[roleName];
+  }
+
+  /**
+  Return the roles assigned to the user. Each item of the returned array will
+  invoke `getRole` with it's item value
+  */
+  function * getUserRoles(ctx) {
+    return userRoles[ctx.identity];
+  }
+
+
+  it('should be a valid provider', function () {
+    [
+      undefined, null, false, true, 0, '', [], {},
+      function () {}, function * () {},
+      { getRolePermissions: function () {} }
+    ].forEach(function (invalidProvider) {
+      +function () { rbac.middleware(invalidProvider); }.should.throw();
+    });
+  });
+
+  it('should be valid permissions', function () {
+    [
+      undefined, null, false, true, 0, '', [], {},
+    ].forEach(function (invalidPermissions) {
+      +function () { rbac.allow(invalidPermissions); }.should.throw();
+      +function () { rbac.deny(invalidPermissions); }.should.throw();
+    });
+  });
+
+  it('should allow / deny', function * () {
+    this.timeout(1000);
+
+    function validate(identity, validateionMiddleware, status) {
+      var app = koa();
+
+      app.use(function * (next) {
+        this.identity = identity;
+
+        yield next;
+      });
+
+      app.use(rbac.middleware(accessProvider));
+      app.use(validateionMiddleware);
+
+      app.use(function * () {
+        this._accessProvider.should.equal(accessProvider);
+        this.status = 200;
+      });
+
+      return function (done) {
+        request(app.listen())
+        .get('/')
+        .expect(status, done);
+      };
+    }
+
+    yield validate('bart', rbac.allow(['read']), 200);
+    yield validate('marge', rbac.allow(['update']), 200);
+
+    yield validate('homer', rbac.allow(['create', 'update']), 200);
+    yield validate('homer', rbac.allow('create', 'update'), 200);
+    yield validate('homer', rbac.allow('create update'), 200);
+
+    yield validate('burns', rbac.allow(['manage']), 200);
+
+    yield validate('homer', rbac.allow(['foo']), 200);
+
+    yield validate('burns', rbac.allow(['read']), 403);
+
+    yield validate('bart', rbac.deny(['read']), 403);
+    yield validate('burns', rbac.deny('read', 'update'), 200);
+    yield validate('burns', rbac.deny('read', 'manage'), 403);
+
+  });
+
+
+  it('should allow / deny if no middleware', function * () {
+    this.timeout(1000);
+
+    function validate(identity, validateionMiddleware, status) {
+      var app = koa();
+
+      app.use(function * (next) {
+        if (identity) {
+          this.identity = identity;
+        }
+        yield next;
+      });
+
+      app.use(validateionMiddleware);
+
+      app.use(function * () {
+        assert.equal(this._accessProvider, undefined);
+        this.status = 200;
+      });
+
+      return function (done) {
+        request(app.listen())
+        .get('/')
+        .expect(status, done);
+      };
+    }
+
+    yield validate(null, rbac.allow('read'), 200);
+    yield validate('bart', rbac.allow(['manage']), 200);
+    yield validate('burns', rbac.allow(['read']), 200);
+
+    yield validate(null, rbac.deny(['foo']), 403);
+    yield validate(null, rbac.deny(['read']), 403);
+    yield validate('bart', rbac.deny(['manage']), 403);
+    yield validate('burns', rbac.deny(['manage']), 403);
+
+  });
+
+
+  it('should allow composed rules', function * () {
+    this.timeout(1000);
+
+    function validate(identity, validateionMiddlewares, status) {
+      var app = koa();
+
+      app.use(function * (next) {
+        this.identity = identity;
+
+        yield next;
+      });
+
+      app.use(rbac.middleware(accessProvider));
+      validateionMiddlewares.forEach(function (validateionMiddleware) {
+        app.use(validateionMiddleware);
+      });
+
+      app.use(function * () {
+        this._accessProvider.should.equal(accessProvider);
+        this.status = 200;
+      });
+
+      return function (done) {
+        request(app.listen())
+        .get('/')
+        .expect(status, done);
+      };
+    }
+
+    yield validate('marge', [ rbac.allow('read update'), rbac.deny('read') ], 200);
+    yield validate('marge', [ rbac.allow('read'), rbac.deny('read') ], 403);
+    yield validate('marge', [ rbac.allow('read'), rbac.allow('update'), rbac.deny('read') ], 200);
+    yield validate('marge', [ rbac.allow('update'), rbac.allow('read'), rbac.deny('read') ], 200);
+    yield validate('marge', [ rbac.allow('update'), rbac.allow('manage'), rbac.deny('read') ], 200);
+
+  });
 
 });
